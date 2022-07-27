d5996471398be60c32e3b6660a25883f6cea8868
KWojcik243 <63014344+KWojcik243@users.noreply.github.com>
2022-07-27 15:21:35 +0200

    Full pdf joiner

---
+++ main.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import filedialog
+from PyPDF2 import PdfFileMerger, PdfFileReader
+
+box = tk.Tk()
+box.withdraw()
+while True:
+    while True:
+        list_files = []
+        list_files = filedialog.askopenfilenames(parent=box,
+                                                 title="Choose files",
+                                                 filetypes=[("PDF files", '*.pdf')])
+        if len(list_files) < 2:
+            action = input("You must select at least two files. Do you want to try again?"
+                           " If so, write 'yes', else write anything, and press enter. \n")
+            if action.lower() != "yes":
+                exit()
+        else:
+            break
+
+    mergedObject = PdfFileMerger()
+    for path in list_files:
+        mergedObject.append(PdfFileReader(path))
+
+    save_file_path = filedialog.asksaveasfilename(title="Choose file name",
+                                                  defaultextension=".pdf")
+    mergedObject.write(save_file_path)
+    action = input("If you want to join other files, write 'yes', else write anything, and press enter. \n")
+    if action.lower() != "yes":
+        break
