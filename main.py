81e65c896febe9aad7f836c8b03d3a461236ec96
KWojcik243 <63014344+KWojcik243@users.noreply.github.com>
2022-07-27 15:43:15 +0200

    Images

--- main.py
+++ main.py
@@ -21,9 +21,17 @@ while True:
     mergedObject = PdfFileMerger()
     for path in list_files:
         mergedObject.append(PdfFileReader(path))
-
-    save_file_path = filedialog.asksaveasfilename(title="Choose file name",
+    while True:
+        save_file_path = filedialog.asksaveasfilename(title="Choose file name",
                                                   defaultextension=".pdf")
+        if save_file_path == '':
+            action = input("You didn't choose any filename. "
+                           "If you want choose filename write 'yes', else write anything, and press enter. \n")
+            if action.lower() != "yes":
+                exit()
+        else:
+            break
+
     mergedObject.write(save_file_path)
     action = input("If you want to join other files, write 'yes', else write anything, and press enter. \n")
     if action.lower() != "yes":
